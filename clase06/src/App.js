511e2f051cafdbf82ddcc8a6631a98f5312c4c7c
AgustinMon <67564027+AgustinMon@users.noreply.github.com>
2022-09-14 18:31:02 -0300

    primera integracion clase 5 a formato react

---
+++ clase06/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <h1>Clase 05</h1>
+        <ul>
+          <li>crear video / boton / audio / lista de elementos.</li>
+          <li>crear input (opcional) que si el ingreso de datos es incorrecto se ponga rojo
+            si no, amarillo y un check al costado</li>
+          <li>Crear un objeto formulario de ingreso que algunos componentes mencionados arriba</li>
+        </ul>
+
+      </header>
+
+      <main>
+        <div id="myApp"></div>
+      </main>
+    </div>
+
+
+  );
+}
+
+export default App;
